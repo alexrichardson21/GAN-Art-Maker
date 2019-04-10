df67645c00ae542f26f6740d520c00a26b039046
alexrichardson21 <richardson.alex101@gmail.com>
2019-04-09 22:23:58 -0700

    formatted the scraper

--- wikiart-scraper.py
+++ wikiart-scraper.py
@@ -1,78 +1,80 @@
-import random
 import time
 import re
-import os
-import argparse
 import urllib
 import urllib.request
 import itertools
 import bs4
 from bs4 import BeautifulSoup
-import multiprocessing
-from multiprocessing.dummy import Pool
 from selenium import webdriver
 
 class WikiartScraper:
-    def get_painting_urls(self, wikiart_profile_url):
-        # random sleep to decrease concurrence of requests
-
-        # "https://www.wikiart.org/en/paintings-by-%s/%s/%d" % (typep, searchword, count)
-        # soup = BeautifulSoup(urllib.request.urlopen(url), "lxml")
+    def scrape_art(self, wikiart_profile_url, output_dir):
+        # opens webpage with selenium
         driver = webdriver.Chrome("./chromedriver73")
         driver.get(wikiart_profile_url)
+
+        # clicks 'LOAD MORE' until all pictures are loaded 
         while(True):
             try:
                 driver.find_element_by_class_name(
             "masonry-load-more-button").click()
                 time.sleep(10)
-            except Exception as e:
+            except:
                 break
         
-        # regex = r'https?://uploads[0-9]+[^/\s]+/\S+\.jpg'
-        # url_list = re.findall(regex, str(driver.page_source))
+        # finds all title blocks from fully expanded webpage
         soup = BeautifulSoup(driver.page_source, "lxml")
         driver.close()
         title_blocks = soup.find_all('div', attrs={'class': 'title-block'})
+
+        # finds all external painting urls from title blocks
         painting_pages = []
         for block in title_blocks:
-            # print(block.text)
             painting_pages += re.findall(r'\/en\/\S+\/\S+\w', str(block))
-        return painting_pages
+        
+        print('Downloading %d images ...' % len(painting_pages))
+        self.download_largest_images(painting_pages, output_dir)
     
-    def largest_images(self, painting_pages):
-        urls = []
+    def download_largest_images(self, painting_pages, output_dir):
         for page in painting_pages:
-            url = "https://www.wikiart.org" + str(page)
-            print(url)
-            soup = BeautifulSoup(urllib.request.urlopen(url), "lxml")
-            block = soup.find('main', attrs={'ng-controller': 'ArtworkViewCtrl'})
-            image_urls = re.findall(
-                r'https?://uploads[0-9]+[^/\s]+/\S+\.jpg', str(block))
-            if image_urls:
-                image_sizes = [self.getsizes(image) for image in image_urls]
-                urls.append(image_urls[image_sizes.index(max(image_sizes))])
-        return urls
-    
-    def getsizes(self, uri):
-        try:
-            file = urllib.request.urlopen(uri)
-            return len(file.read())
-        except:
-            return 0
-    
-    def downloader(self, links, output_dir):
-        savepath = output_dir
-        for link in links:
+            url = "https://www.wikiart.org%s" % str(page)
+            print('Finding best image from %s' % url)
+
             try:
-                time.sleep(0.2)  # try not to get a 403
-                regex = r'((.(?<!\/))+$)'
-                print(re.findall(regex, link)[0][0])
-                urllib.request.urlretrieve(link, savepath + re.findall(regex, link)[0][0])
+                # finds the ArtworkViewCtrl block
+                soup = BeautifulSoup(urllib.request.urlopen(url), "lxml")
+                block = soup.find('main', attrs={'ng-controller': 'ArtworkViewCtrl'})
+
+                # finds all image urls for painting
+                image_urls = re.findall(
+                    r'https?://uploads[0-9]+[^/\s]+/\S+\.jpg', str(block))
+                if image_urls:
+                    # finds the largest sized image of painting
+                    image_sizes = [self.get_size(image) for image in image_urls]
+                    largest_image = image_urls[image_sizes.index(max(image_sizes))]
+
+                    # downloads largest image
+                    self.download_image_to_dir(largest_image, output_dir)
+            
             except Exception as e:
-                print("failed downloading ", e)
+                print("failed downloading", e)
+    
+    def get_size(self, url):
+        # gets size of image from url in bytes
+        file = urllib.request.urlopen(url)
+        return len(file.read())
+    
+    def download_image_to_dir(self, link, output_dir):
+        try:
+            regex = r'((.(?<!\/))+$)'
+            filename = re.findall(regex, link)[0][0]
+            print('Downloading %s' % filename)
+            urllib.request.urlretrieve(link, output_dir + filename)
+        except Exception as e:
+            print("failed downloading ", e)
         
 if __name__ == '__main__':
     ws = WikiartScraper()
-    urls = ws.get_painting_urls("https://www.wikiart.org/en/profile/5c9ba655edc2c9b87424edfe/albums/favourites")
-    images = ws.largest_images(urls)
-    ws.downloader(images, './select_train/')
+    profile_url = 'https://www.wikiart.org/en/profile/5c9ba655edc2c9b87424edfe/albums/favourites'
+    output_dir = './select_train/'
+    ws.scrape_art(profile_url, output_dir)
