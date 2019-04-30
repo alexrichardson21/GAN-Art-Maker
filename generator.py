d93f34d692368cd50b96816b8c7f5e35c0a6c202
alexrichardson21 <richardson.alex101@gmail.com>
2019-04-29 17:39:24 -0700

    formatting

--- generator.py
+++ generator.py
@@ -1,19 +1,27 @@
 import argparse
 
+import matplotlib.pyplot as plt
 import numpy as np
 from keras.models import load_model
 from PIL import Image
 
 
 def generate(n, model_filepath):
-    noise_shape = 100
-    noise = np.random.normal(0, 1, (n, noise_shape))
+    noise = np.random.normal(0, 1, (n, 100))
     model = load_model(model_filepath)
-    gen_art_data = model.predict(noise)
 
+    gen_art_data = model.predict(noise)
+    # Rescale images 0 - 1
+    gen_art_data = 0.5 * gen_art_data + 0.5
+
+    # Saves n generated images
     for i, art_data in enumerate(gen_art_data):
-        art_data = ((art_data + 127.5) * 127.5).astype(np.int16)
-        Image.fromarray(art_data, mode='RGB').save('./gen_art/art_%d.jpg' % i)
+        fig, axs = plt.subplots()
+        axs.imshow(art_data)
+        axs.axis('off')
+        fig.savefig('gen_art/art_%d.jpg' % i, 
+                    bbox_inches='tight', pad_inches=0)
+        plt.close()
 
 
 def parse_command_line_args():
@@ -21,10 +29,11 @@ def parse_command_line_args():
     parser.add_argument('N', type=int,
                         help='number of images to generate and save')
     parser.add_argument('path', type=str,
-                        help='filepath to pretrained h5 Keras model')
+                        help='filepath to pretrained h5 generator')
     return vars(parser.parse_args())
 
 
 if __name__ == '__main__':
     args = parse_command_line_args()
+    # generate(10, 'art_gan_generator.h5')
     generate(args['N'], args['path'])
